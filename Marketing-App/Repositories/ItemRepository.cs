67fac85581e12fd5a083e5c9a245fe62967684b2
Marzie70 <mkazemi_1370@yahoo.com>
2019-09-30 16:21:05 +0330

    AddFolderRepository

---
+++ Marketing-App/Repositories/ItemRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes.Interfaces;
+using Classes.MappingClasses;
+using NHibernate;
+
+namespace Marketing_App.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private ISession session;
+        public IQueryable<Item> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Item obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Item obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Item obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+//public void Delete(Classes.MappingClasses.Item obj)
